5f2ccb47c757ae60fd201a238121145661d00134
김동현 <donghyeon@DongHyeon-MacBookPro-2.local>
2024-09-15 19:50:16 +0900

    second commit

--- project/port/build_linux_all.sh
+++ project/port/build_linux_all.sh
@@ -11,41 +11,45 @@ build_in_dir() {
 }
 
 
-build_in_dir "c/c-HTTP/linux-std"
-build_in_dir "c/c-HTTP/linux-x86-32"
-build_in_dir "c/c-HTTP/linux-x86-ubc222-yocto"
+# build_in_dir "c/c-HTTP/linux-std"
+# build_in_dir "c/c-HTTP/linux-x86-32"
+build_in_dir "c/c-HTTP/linux-x86-64"
+# build_in_dir "c/c-HTTP/linux-x86-ubc222-yocto"
 
-build_in_dir "c/c-MQTT/linux-std"
-build_in_dir "c/c-MQTT/linux-x86-32"
-build_in_dir "c/c-MQTT/linux-x86-ubc222-yocto"
+# build_in_dir "c/c-MQTT/linux-std"
+# build_in_dir "c/c-MQTT/linux-x86-32"
+build_in_dir "c/c-MQTT/linux-x86-64"
+# build_in_dir "c/c-MQTT/linux-x86-ubc222-yocto"
 
-build_in_dir "c/c-TCP/linux-std"
-build_in_dir "c/c-TCP/linux-x86-32"
-build_in_dir "c/c-TCP/linux-x86-ubc222-yocto"
+# build_in_dir "c/c-TCP/linux-std"
+# build_in_dir "c/c-TCP/linux-x86-32"
+build_in_dir "c/c-TCP/linux-x86-64"
+# build_in_dir "c/c-TCP/linux-x86-ubc222-yocto"
 
-build_in_dir "c/c-TLS/linux-std"
-build_in_dir "c/c-TLS/linux-x86-32"
-build_in_dir "c/c-TLS/linux-x86-ubc222-yocto"
-
-
-
-
-build_in_dir "java/java-TCP/android-arm-32"
-build_in_dir "java/java-TCP/android-arm-64"
+# build_in_dir "c/c-TLS/linux-std"
+# build_in_dir "c/c-TLS/linux-x86-32"
+build_in_dir "c/c-TLS/linux-x86-64"
+# build_in_dir "c/c-TLS/linux-x86-ubc222-yocto"
 
 
-build_in_dir "java/java-TCP/linux-x86-32"
-build_in_dir "java/java-TCP/linux-x86-64"
 
 
-build_in_dir "java/java-HTTP/linux-x86-32"
-build_in_dir "java/java-HTTP/linux-x86-64"
+# build_in_dir "java/java-TCP/android-arm-32"
+# build_in_dir "java/java-TCP/android-arm-64"
 
 
-build_in_dir "java/java-MQTT/linux-x86-32"
-build_in_dir "java/java-MQTT/linux-x86-64"
+# build_in_dir "java/java-TCP/linux-x86-32"
+# build_in_dir "java/java-TCP/linux-x86-64"
 
 
-build_in_dir "java/java-TLS/linux-x86-32"
-build_in_dir "java/java-TLS/linux-x86-64"
+# build_in_dir "java/java-HTTP/linux-x86-32"
+# build_in_dir "java/java-HTTP/linux-x86-64"
+
+
+# build_in_dir "java/java-MQTT/linux-x86-32"
+# build_in_dir "java/java-MQTT/linux-x86-64"
+
+
+# build_in_dir "java/java-TLS/linux-x86-32"
+# build_in_dir "java/java-TLS/linux-x86-64"
 
