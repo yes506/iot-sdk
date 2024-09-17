da0eb7f15a471101ef77b6a4dfc3500aa32bf037
김동현 <donghyeon@DongHyeon-MacBookPro-2.local>
2024-09-17 11:16:05 +0900

    fifth commit

--- project/port/build_linux_all.sh
+++ project/port/build_linux_all.sh
@@ -11,14 +11,14 @@ build_in_dir() {
 }
 
 
-build_in_dir "c/c-HTTP/linux-std"
+# build_in_dir "c/c-HTTP/linux-std"
 # build_in_dir "c/c-HTTP/linux-x86-32"
-build_in_dir "c/c-HTTP/linux-x86-64"
+# build_in_dir "c/c-HTTP/linux-x86-64"
 # build_in_dir "c/c-HTTP/linux-x86-ubc222-yocto"
 
-build_in_dir "c/c-MQTT/linux-std"
+# build_in_dir "c/c-MQTT/linux-std"
 # build_in_dir "c/c-MQTT/linux-x86-32"
-build_in_dir "c/c-MQTT/linux-x86-64"
+# build_in_dir "c/c-MQTT/linux-x86-64"
 # build_in_dir "c/c-MQTT/linux-x86-ubc222-yocto"
 
 build_in_dir "c/c-TCP/linux-std"
@@ -26,9 +26,9 @@ build_in_dir "c/c-TCP/linux-std"
 build_in_dir "c/c-TCP/linux-x86-64"
 # build_in_dir "c/c-TCP/linux-x86-ubc222-yocto"
 
-build_in_dir "c/c-TLS/linux-std"
+# build_in_dir "c/c-TLS/linux-std"
 # build_in_dir "c/c-TLS/linux-x86-32"
-build_in_dir "c/c-TLS/linux-x86-64"
+# build_in_dir "c/c-TLS/linux-x86-64"
 # build_in_dir "c/c-TLS/linux-x86-ubc222-yocto"
 
 
